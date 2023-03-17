eba3bde5c0490b265f4c4903f02e895da48a0bf1
Maze <logins@choutka.ml>
2023-03-17 22:56:56 +0100

    sandboxrc bad

--- dotconfig/zsh.d/20_alias.zsh
+++ dotconfig/zsh.d/20_alias.zsh
@@ -44,3 +44,27 @@ alias -s org=nvim
 alias -s zip=nvim
 alias -s tar=nvim
 #alias -s tar.{xz,gz,bz,bz2}=nvim
+
+## NVM lazyload
+### https://broken-by.me/lazy-load-nvm/
+nvm() {
+    unset -f nvm
+    export NVM_DIR=~/.nvm
+    [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+    nvm "$@"
+}
+
+node() {
+    unset -f node
+    export NVM_DIR=~/.nvm
+    [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+    node "$@"
+}
+
+npm() {
+    unset -f npm
+    export NVM_DIR=~/.nvm
+    [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+    npm "$@"
+}
+
