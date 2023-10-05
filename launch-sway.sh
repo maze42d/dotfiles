603eb2b482ad4601397a6858aa6fb4474730b747
maze-designs <22244736+maze-designs@users.noreply.github.com>
2023-10-05 23:09:02 +0200

    more sway variables

--- launch-sway.sh
+++ launch-sway.sh
@@ -3,11 +3,13 @@
 export SDL_VIDEODRIVER=wayland
 export LIBVA_DRIVER_NAME=nvidia
 export XDG_SESSION_TYPE=wayland
+export XDG_CURRENT_DESKTOP=sway
 export GBM_BACKEND=nvidia-drm
 export GDK_BACKEND=wayland
 export __GLX_VENDOR_LIBRARY_NAME=nvidia
 export WLR_RENDERER=vulkan
 export WLR_NO_HARDWARE_CURSORS=1
+export RTC_USE_PIPEWIRE=true
 export MOZ_ENABLE_WAYLAND=1
 #export XWAYLAND_NO_GLAMOR=1
 
