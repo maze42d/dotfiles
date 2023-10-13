e96d987b45006b5891ed14976d76e2b4622781de
maze-designs <22244736+maze-designs@users.noreply.github.com>
2023-10-13 18:30:20 +0200

    more wofi prompts

---
+++ wofi-scripts/anything_and_everything.rb
@@ -0,0 +1,24 @@
+options = {
+  "Service: libvirt start" => 'pkexec systemctl start libvirtd',
+  "Service: libvirt stop" => 'pkexec systemctl stop libvirtd',
+  "Wireshark: USB monitor enable" => 'pkexec zsh -c "modprobe usbmon; chmod 777 /dev/usbmon*"',
+  "Wireshark: USB monitor disable" => 'pkexec zsh -c "rmmod usbmon"',
+  "Pulsemixer" => 'kitty pulsemixer',
+
+}
+
+opt_string = ""
+options.each do |k, v|
+  opt_string += k + "\n"
+end
+
+opt_string.slice!((opt_string.length - 1))
+
+dmenu = `echo "#{opt_string}" | wofi --dmenu`
+dmenu = dmenu.split("\n")
+
+#puts "there it goes: "
+#puts dmenu[-1]
+#puts "so this should run:"
+puts "running: ", options[dmenu[-1]]
+`#{options[dmenu[-1]]}`
